f2d726d76b0b3b096c8bddac229a1890395429af
egehiwh <108092476+egehiwh@users.noreply.github.com>
2023-11-09 15:54:14 +0200

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -3,14 +3,16 @@ let handler = async m => {
 }
 
 handler.all = async function (m) {
-  // Always send a fake recording status for any received message
-  this.sendPresenceUpdate('recording', m.chat);
+  try {
+    // Always send a fake recording status for any received message
+    this.sendPresenceUpdate('recording', m.chat);
 
-  // You can add additional code here if needed
-  
-  // Wait for 10 seconds
-  await new Promise(resolve => setTimeout(resolve, 10000));
-  
-  // Optionally, you can send another message after the 10 seconds
-  // this.sendMessage(m.chat, 'Recording completed!');
+    // Wait for 10 seconds
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    // Optionally, you can send another message after the 10 seconds
+    // await this.sendMessage(m.chat, 'Recording completed!');
+  } catch (error) {
+    console.error(error);
+  }
 }
