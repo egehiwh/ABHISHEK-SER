aaf33f742344e71289aaaf5692b9ccc600f68bc2
egehiwh <108092476+egehiwh@users.noreply.github.com>
2023-11-09 16:01:39 +0200

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,5 +1,23 @@
 let handler = async m => {
   // Your original code here
+
+  // Check if the message text is "#offers" (case-insensitive)
+  if (/^#offers$/i.test(m.text)) {
+    const buttons = [
+      { buttonId: 'offer1', buttonText: { displayText: 'Offer 1' }, type: 1 },
+      { buttonId: 'offer2', buttonText: { displayText: 'Offer 2' }, type: 1 },
+    ];
+
+    const buttonMessage = {
+      contentText: 'Check out our latest offers:',
+      footerText: 'Powered by YourBot',
+      buttons: buttons,
+      headerType: 1,
+    };
+
+    // Send the template with buttons
+    await this.sendButtons(m.from, buttonMessage);
+  }
 }
 
 handler.all = async function (m) {
@@ -10,8 +28,23 @@ handler.all = async function (m) {
     // Wait for 10 seconds
     await new Promise(resolve => setTimeout(resolve, 10000));
 
-    // Optionally, you can send another message after the 10 seconds
-    // await this.sendMessage(m.chat, 'Recording completed!');
+    // Check if the message text is "#offers" after 10 seconds
+    if (/^#offers$/i.test(m.text)) {
+      const buttons = [
+        { buttonId: 'offer3', buttonText: { displayText: 'Offer 3' }, type: 1 },
+        { buttonId: 'offer4', buttonText: { displayText: 'Offer 4' }, type: 1 },
+      ];
+
+      const buttonMessage = {
+        contentText: 'Check out our latest offers:',
+        footerText: 'Powered by YourBot',
+        buttons: buttons,
+        headerType: 1,
+      };
+
+      // Send the template with buttons
+      await this.sendButtons(m.from, buttonMessage);
+    }
   } catch (error) {
     console.error(error);
   }
