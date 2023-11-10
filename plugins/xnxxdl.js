70d66b30c1067c54ef263b6c4a6d80c3aa62f2b0
egehiwh <108092476+egehiwh@users.noreply.github.com>
2023-11-11 00:25:14 +0200

    Update xnxxdl.js

--- plugins/xnxxdl.js
+++ plugins/xnxxdl.js
@@ -1,63 +1,60 @@
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!db.data.chats[m.chat].nsfw && m.isGroup) throw '⚠️This Group Doesnt Supprt Nsfw Enable It By *.on nsfw*';
-  if (!args[0]) throw `⚠️Give A Valid Url *📌Example: ${usedPrefix + command} https://www.xnxx.com/video-14lcwbe8/rubia_novia_follada_en_cuarto_de_bano*`;
-  try {
-    await conn.reply(m.chat, '*✅The Video Is Processed Wait Sometime..*\n\n﹣ *⌛The Downloading Time Depends On The Duration Of The Video*', m);
-    let xnxxLink = '';
-    if (args[0].includes('xnxx')) {
-      xnxxLink = args[0];
-    } else {
-      const index = parseInt(args[0]) - 1;
-      if (index >= 0) {
-        if (Array.isArray(global.videoListXXX) && global.videoListXXX.length > 0) {
-          const matchingItem = global.videoListXXX.find((item) => item.from === m.sender);
-          if (matchingItem) {
-            if (index < matchingItem.urls.length) {
-              xnxxLink = matchingItem.urls[index];
-            } else {
-              throw `* ${matchingItem.urls.length}*`;
-            }
-          } else {
-            throw `*🎯To Be Able To Use This Command In This Way (${usedPrefix + command} <numero>), Please Use Videos With The Command ${usedPrefix}xnxxsearch <texto>*`;
-          }
-        } else {
-          throw `*🎯To Be Able To Use This Command In This Way (${usedPrefix + command} <numero>), Please Use Videos With The Command ${usedPrefix}xnxxsearch <texto>*`;
-        }
-      }
-    }
-    const res = await xnxxdl(xnxxLink);
-    const json = await res.result.files;
-    conn.sendMessage(m.chat, {document: {url: json.high}, mimetype: 'video/mp4', fileName: res.result.title}, {quoted: m});
-  } catch {
-    throw '*⚠️InValid Url* *Give A Valid Url* *📌Example: .xnxxdl https://www.xnxx.com/video-14lcwbe8/rubia_novia_follada_en_cuarto_de_bano*';
-  }
-};
-handler.command = /^(xnxxdl)$/i;
-export default handler;
+import { youtubedlv2, youtubedlv3, youtubedl } from '@bochilteam/scraper'
 
-async function xnxxdl(URL) {
-  return new Promise((resolve, reject) => {
-    fetch(`${URL}`, {method: 'get'}).then((res) => res.text()).then((res) => {
-      const $ = cheerio.load(res, {xmlMode: false});
-      const title = $('meta[property="og:title"]').attr('content');
-      const duration = $('meta[property="og:duration"]').attr('content');
-      const image = $('meta[property="og:image"]').attr('content');
-      const videoType = $('meta[property="og:video:type"]').attr('content');
-      const videoWidth = $('meta[property="og:video:width"]').attr('content');
-      const videoHeight = $('meta[property="og:video:height"]').attr('content');
-      const info = $('span.metadata').text();
-      const videoScript = $('#video-player-bg > script:nth-child(6)').html();
-      const files = {
-        low: (videoScript.match('html5player.setVideoUrlLow\\(\'(.*?)\'\\);') || [])[1],
-        high: videoScript.match('html5player.setVideoUrlHigh\\(\'(.*?)\'\\);' || [])[1],
-        HLS: videoScript.match('html5player.setVideoHLS\\(\'(.*?)\'\\);' || [])[1],
-        thumb: videoScript.match('html5player.setThumbUrl\\(\'(.*?)\'\\);' || [])[1],
-        thumb69: videoScript.match('html5player.setThumbUrl169\\(\'(.*?)\'\\);' || [])[1],
-        thumbSlide: videoScript.match('html5player.setThumbSlide\\(\'(.*?)\'\\);' || [])[1],
-        thumbSlideBig: videoScript.match('html5player.setThumbSlideBig\\(\'(.*?)\'\\);' || [])[1]};
-      resolve({status: 200, result: {title, URL, duration, image, videoType, videoWidth, videoHeight, info, files}});
-    }).catch((err) => reject({code: 503, status: false, result: err}));
-  });
+const handler = async (m, { conn, args, command }) => {
+  if (!args[0]) throw 'Where`s Url?'
+  const v = args[0]
+
+  const resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
+  let qu = args[1] && resolutions.includes(args[1]) ? args[1] : "360p"
+  let q = qu.replace('p', '')
+
+  let thumb = {}
+  try {
+    const thumb2 = yt.thumbnails[0].url
+    thumb = { jpegThumbnail: thumb2 }
+  } catch (e) {}
+
+  let yt
+  try {
+    yt = await youtubedl(v)
+  } catch {
+    yt = await youtubedlv2(v)
+  }
+
+  const title = await yt.title
+
+  let size = ''
+  let dlUrl = ''
+  let selectedResolution = ''
+  let selectedQuality = ''
+  for (let i = resolutions.length - 1; i >= 0; i--) {
+    const res = resolutions[i]
+    if (yt.video[res]) {
+      selectedResolution = res
+      selectedQuality = res.replace('p', '')
+      size = await yt.video[res].fileSizeH
+      dlUrl = await yt.video[res].download()
+      break
+    }
+  }
+
+  if (dlUrl) {
+    await	m.react('🎥')
+    await m.reply(`YouTube video download request. Processing, please be patient...`)
+
+    await conn.sendMessage(m.chat, { video: { url: dlUrl, caption: title, ...thumb } }, { quoted: m })
+
+    await m.reply(`▢ Title: ${title}
+▢ Resolution: ${selectedResolution}
+▢ Size: ${size}
+▢ The video has been successfully downloaded!\n\n${fig}`)
+  } else {
+    await m.reply(`Sorry, the video is not available for download.`)
+  }
 }
+
+handler.command = /^(getvid|ytmp4|youtubemp4|ytv|video)$/i
+handler.help = ["getvid <linkYt>","ytmp4 <linkYT>"]
+handler.tags = ['downloader']
+
+export default handler
